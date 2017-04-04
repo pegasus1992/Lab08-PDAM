ef7dfc38bb1acd24cd92d1d87999c35676e0178c
estudiante <estudiante@ismac08.local>
2017-04-04 17:36:31 -0500

    commit

---
+++ app/src/main/java/com/edu/eci/pdam/lab08/AddressResultListener.java
@@ -0,0 +1,8 @@
+package com.edu.eci.pdam.lab08;
+
+/**
+ * @author Santiago Carrillo
+ */
+public interface AddressResultListener {
+    void onAddressFound(String address);
+}
--- app/src/main/java/com/edu/eci/pdam/lab08/MapsActivity.java
+++ app/src/main/java/com/edu/eci/pdam/lab08/MapsActivity.java
@@ -1,8 +1,25 @@
 package com.edu.eci.pdam.lab08;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.TextView;
 
+import com.edu.eci.pdam.lab08.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,18 +27,31 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private GoogleMap mMap;
+    private static final int ACCESS_LOCATION_PERMISSION_CODE = 10;
+
+    private final LocationRequest locationRequest = new LocationRequest();
+    private GoogleMap googleMap;
+    private GoogleApiClient googleApiClient;
+    private TextView address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        address = (TextView) findViewById(R.id.text);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Configure Google Maps API Objects
+        googleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        googleApiClient.connect();
     }
 
 
@@ -36,11 +66,119 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        this.googleMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //this.googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @SuppressWarnings("MissingPermission")
+    public void showMyLocation() {
+        if (googleMap != null) {
+            String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION};
+            if (hasPermissions(this, permissions)) {
+                googleMap.setMyLocationEnabled(true);
+
+
+                Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+                if (lastLocation != null) {
+                    addMarkerAndZoom(lastLocation, "My Location", 15);
+                }
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, ACCESS_LOCATION_PERMISSION_CODE);
+            }
+        }
+    }
+
+
+    public static boolean hasPermissions(Context context, String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_DENIED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addMarkerAndZoom(Location location, String title, int zoom) {
+        LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        googleMap.addMarker(new MarkerOptions().position(myLocation).title(title));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, zoom));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_LOCATION_PERMISSION_CODE:
+                showMyLocation();
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        stopLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        startLocationUpdates();
+    }
+
+    @SuppressWarnings("MissingPermission")
+    public void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                showMyLocation();
+                stopLocationUpdates();
+            }
+        });
+    }
+
+    public void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+        });
+    }
+
+    public void onFindAddressClicked(View view) {
+        startFetchAddressIntentService();
+    }
+
+
+    @SuppressWarnings("MissingPermission")
+    public void startFetchAddressIntentService() {
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (lastLocation != null) {
+            AddressResultReceiver addressResultReceiver = new AddressResultReceiver(new Handler());
+            addressResultReceiver.setAddressResultListener(new AddressResultListener() {
+                @Override
+                public void onAddressFound(final String address) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            MapsActivity.this.address.setText(address);
+                            MapsActivity.this.address.setVisibility(View.VISIBLE);
+                        }
+                    });
+                }
+            });
+            Intent intent = new Intent(this, FetchAddressIntentService.class);
+            intent.putExtra(FetchAddressIntentService.RECEIVER, addressResultReceiver);
+            intent.putExtra(FetchAddressIntentService.LOCATION_DATA_EXTRA, lastLocation);
+            startService(intent);
+        }
     }
 }
